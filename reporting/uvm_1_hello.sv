a557f2e0460d7def985560344bd612e9aa4420b6
skb_kochi <skandakrishnan96@gmail.com>
2024-11-22 18:05:39 -0500

    Add files via upload

---
+++ reporting/uvm_1_hello.sv
@@ -0,0 +1,13 @@
+// Code your testbench here
+// or browse Examples
+
+
+`include "uvm_macros.svh"
+import uvm_pkg ::*;
+
+module tb;
+  initial begin
+    `uvm_info("TB_TOP", "Hello World", UVM_NONE);
+  end
+endmodule
+
